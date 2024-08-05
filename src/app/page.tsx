8e7b809d87ad38f1f75a1a7471bf8e51877cafbb
Mariano Eiberman <mariano.eiberman@gmail.com>
2024-08-05 17:33:50 -0300

    Change styles to main screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   const mainPassword: string = process.env.MAIN_PASSWORD || "";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="z-10 w-full flex-col max-w-5xl items-center justify-between font-mono text-lg lg:flex">
+        <h1 className="fixed left-0 top-0 flex w-full my-8 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Welcome! please login to use the Money app!
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        </h1>
+        <div className="flex p-8 rounded-3xl bg-black drop-shadow-xl ">
+        <Login envs={{ mainUser, mainPassword}}/>
         </div>
       </div>
-      <Login envs={{ mainUser, mainPassword}}/>
     </main>
   );
 }
