0a86077a86c92b1ea43c133acc7892445a403551
Mariano Eiberman <mariano.eiberman@gmail.com>
2024-08-06 12:59:01 -0300

    fix styles on layout and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ export default function Home() {
   const mainPassword: string = process.env.MAIN_PASSWORD || "";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full flex-col max-w-5xl items-center justify-between font-mono text-lg lg:flex">
-        <h1 className="fixed left-0 top-0 flex w-full my-8 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="z-10 w-full flex-col max-w-5xl items-center lg:flex">
+        <div className="flex flex-row items-center text-7xl w-auto"><Image src={'/logo.svg'} className="mx-5" alt="logo" width={100} height={100} />EIBERTEK</div>
+        <h1 className="w-full my-8 justify-center flex border-b text-4xl border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Welcome! please login to use the Money app!
         </h1>
-        <div className="flex p-8 rounded-3xl bg-black drop-shadow-xl ">
+        <div className="flex p-8 rounded-3x1 ">
         <Login envs={{ mainUser, mainPassword}}/>
         </div>
       </div>
--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -19,12 +19,18 @@ export const Login = ({envs}:{envs: {mainUser: string, mainPassword:string}}) =>
     };
 
     return (
-        <section className="w-full text-center">
+        <section className="w-full text-center text-3xl">
         <div>Login</div>
         {error && <div>{error}</div>}
-        <div className="py-4"><label>Username: </label><input name="username"className="text-black"  onChange={setInput}/></div>
-        <div><label>Password: </label><input type="password" name="password" className="text-black" onChange={setInput}/></div>
-        <div><button onClick={loginAction} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-6 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Enter</button></div>
+        <div className="py-4">
+            <label className='mb-2'>Username: </label>
+            <input name="username"className="text-black rounded-full p-5"  onChange={setInput}/>
+        </div>
+        <div>
+            <label className='mb-2'>Password: </label>
+            <input type="password" name="password" className="text-black rounded-full p-5" onChange={setInput}/>
+        </div>
+        <div><button onClick={loginAction} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full w-full px-8 py-5 me-2 my-6 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 shadow-lg">Enter</button></div>
         </section>
     );
 }
