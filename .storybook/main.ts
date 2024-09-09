11607aa6c7825376767dd35d889303d4841b9543
Mariano Eiberman <mariano.eiberman@gmail.com>
2024-09-09 15:51:48 -0300

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@chakra-ui/storybook-addon",
+  ],
+  features:{
+    emotionAlias:false,
+  },
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["..\\public"],
+};
+export default config;
