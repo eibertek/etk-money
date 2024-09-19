9d260191d8c3a0325a4eedd18126a6ba687a7cdd
Mariano Eiberman <mariano.eiberman@gmail.com>
2024-09-19 15:07:40 -0300

    fix bulk dropdown

--- src/components/move/bulkNew.tsx
+++ src/components/move/bulkNew.tsx
@@ -5,11 +5,13 @@ import crypto from "crypto";
 import { Grid } from "../shared/grid";
 import Input from '@/components/shared/forms/input';
 import Dropdown from '@/components/shared/forms/dropdown';
+import BulkDropdown from '@/components/shared/forms/bulk_dropdown';
 import DateComponent from '@/components/shared/forms/date';
-import FormComponent, { FormPropsContext } from '@/components/shared/forms/form';
+import FormComponent from '@/components/shared/forms/form';
 import { ModalContext } from "../shared/dialog/modalContext";
 import ButtonLink from "../shared/button/LinkButton";
 import { ALLOW_EMPTY } from "../shared/constants";
+import { Flex } from "@chakra-ui/react";
 
 interface IBulkNewProps {
     setModalOpen?: any;
@@ -19,21 +21,21 @@ const ENTITY_NAME = 'move';
 
 const BulkNew = ({ setModalOpen = () => { } }: IBulkNewProps) => {
     const [rowNum, setRowNum] = useState(1);
-    const [clients, setClients] = useState([]);
-    const [currencies, setCurrencies] = useState([]);
+    const [clients, setClients] = useState([] as Client[]);
+    const [currencies, setCurrencies] = useState([] as Currency[]);
     const [message, setMessage] = useState([]);
     const modal = useContext(ModalContext);
 
     useEffect(() => {
-        const clients = storageHook('client').getAll();
-        const currencies = storageHook('currencies').getAll();
+        const clients: Client[] = storageHook('client').getAll();
+        const currencies: Currency[] = storageHook('currencies').getAll();
         setCurrencies(currencies);
         setClients(clients);
     }, [modal]);
 
     const clientOptions = clients.map(({ id, name }) => ({ id, label: name }));
     const currencyOptions = currencies.map(({ id }) => ({ id, label: id }));
-
+    const typeOptions = [{ id: 'I', label: 'Income' }, { id: 'O', label: 'Outcome' }];
     const saveMove = (move: Move) => {
         // create unique id hash
         const hmacId = crypto.createHmac('sha256', `${move.client}_${move.currency}_${move.date}_${move.amount}_${move.type}`);
@@ -78,17 +80,24 @@ const BulkNew = ({ setModalOpen = () => { } }: IBulkNewProps) => {
                         save(values);
                     }}
                 >
-                    <Grid cols={['Date', 'Account', 'Currency', 'Income', 'Type']}>
-                        {Array(rowNum).fill(1).map((_a, i) => {
-                            return [
-                                <DateComponent key={`field_${i}_date`} noLabel field={`${i}_date`} />,
-                                <Dropdown noLabel key={`field_${i}_client`} options={clientOptions} field={`${i}_client`} />,
-                                <Dropdown noLabel key={`field_${i}_currency`} options={currencyOptions} field={`${i}_currency`} />,
-                                <Input noLabel key={`field_${i}_amount`} type="number" field={`${i}_amount`} />,
-                                <Dropdown noLabel field={`${i}_type`} key={`field_${i}_type`} options={[{ id: 'I', label: 'Income' }, { id: 'O', label: 'Outcome' }]} validationRules={{ [ALLOW_EMPTY]: false }} />,
-                            ];
-                        }).flat()}
-                    </Grid>
+                    <>
+                        <Flex direction={"row"} gap={2} mb={4}>
+                            <BulkDropdown key={`bulk_client`} options={clientOptions} field={`bulk_client`} destiny="client" rowNum={rowNum} />
+                            <BulkDropdown key={`bulk_currency`} options={currencyOptions} field={`bulk_currency`} destiny="currency" rowNum={rowNum} />
+                            <BulkDropdown key={`bulk_type`} options={typeOptions} field={`bulk_type`} destiny="type" rowNum={rowNum} />
+                        </Flex>
+                        <Grid cols={['Date', 'Account', 'Currency', 'Income', 'Type']}>
+                            {Array(rowNum).fill(1).map((_a, i) => {
+                                return [
+                                    <DateComponent key={`field_${i}_date`} noLabel field={`${i}_date`} />,
+                                    <Dropdown noLabel key={`field_${i}_client`} options={clientOptions} field={`${i}_client`} />,
+                                    <Dropdown noLabel key={`field_${i}_currency`} options={currencyOptions} field={`${i}_currency`} />,
+                                    <Input noLabel key={`field_${i}_amount`} type="number" field={`${i}_amount`} />,
+                                    <Dropdown noLabel field={`${i}_type`} key={`field_${i}_type`} options={typeOptions} validationRules={{ [ALLOW_EMPTY]: false }} />,
+                                ];
+                            }).flat()}
+                        </Grid>
+                    </>
                 </FormComponent>
                 <div>{message}</div>
             </div>
--- src/components/shared/forms/bulk_dropdown/index.tsx
+++ src/components/shared/forms/bulk_dropdown/index.tsx
@@ -11,14 +11,16 @@ interface InputProps {
     field: string;
     options: {id:string, label?:string}[];
     noLabel?: boolean;
+    destiny?: string;
+    rowNum?: number;
     validationRules?: {
         [ALLOW_EMPTY]?: boolean,
         [MAX_LENGTH]?: number
     };  
 };
 
-export const Component = ({ field, options, noLabel=false, validationRules = { [ALLOW_EMPTY]:true} }:InputProps) => {
-    const {errors, values}:any = useContext(FormPropsContext as Context<unknown>);
+export const Component = ({ field, options, noLabel=false, validationRules = { [ALLOW_EMPTY]:true}, destiny, rowNum=0 }:InputProps) => {
+    const {errors, values, setValues}:any = useContext(FormPropsContext as Context<unknown>);
    
     const validation = (value:string) => {
         if(!validationRules[ALLOW_EMPTY] && (!value || value === "")) {
@@ -26,12 +28,21 @@ export const Component = ({ field, options, noLabel=false, validationRules = { [
         };
     };
     const items = options.map(({ id, label}, index) => <option key={`${field}_option_${id}_${index}`} value={id}>{label}</option>)
+    const handleOnChange = (opts: any, onChangeFn: any) => {
+        const newFieldsValues = {...values};
+        for (let c = 0; c < rowNum; c++) {
+            newFieldsValues[`${c}_${destiny}`]= opts?.target.value;
+        }
+        setValues(newFieldsValues);
+        onChangeFn(opts);
+    };
+
     return (
         <Field name={field} validate={validation}>
         {({ field: fieldProps }: FieldProps) => (
             <FormControl>
-                {!noLabel && <FormLabel style={{ textTransform: "capitalize"}}>{field}</FormLabel>}
-                <Select name={field} icon={<ArrowDownIcon />} placeholder="Select from the list" value={values && values[field]} onChange={fieldProps.onChange}>{items}</Select>
+                {!noLabel && <FormLabel style={{ textTransform: "capitalize"}}>{field.replace("_", " ")}</FormLabel>}
+                <Select name={field} icon={<ArrowDownIcon />} placeholder="Select from the list" value={values && values[field]} onChange={(opts)=>handleOnChange(opts, fieldProps.onChange)}>{items}</Select>
                 {errors && errors[field] && <Box color="tomato" >{errors[field]}</Box> }
             </FormControl>
         )}
